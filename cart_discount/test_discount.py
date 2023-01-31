fca49487fc0c5f78beb0c310e9d1a9be66fd459a
Owner <abdc>
2023-01-31 14:39:19 -0600

    test_discount that discount returns the correct price when there are four items

--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -12,6 +12,23 @@ class TestDiscount(TestCase):
     
     # TODO more unit tests here. Each test should test one scenario
 
+    def test_discount_less_than_three(self):
+
+        """Test that discount returns the correct price when there are less than three items"""
+
+        prices = [10,20]
+        expected_discount = 4
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_discount_more_than_three(self):
+
+        """Test that discount returns the correct price when there are four items"""
+
+        prices = [10, 4,25,20]
+        expected_discount = 4
+        self.assertEqual(expected_discount, discount(prices))
+        
+
 
 if __name__ == '__main__':
     unittest.main()
